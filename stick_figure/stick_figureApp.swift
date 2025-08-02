8079492d6628411da80da0e20ae2024988626166
Liu Weimi Chris <notexttospeachfmsp@gmail.com>
2025-08-02 09:56:10 +0800

    Initial Commit

---
+++ stick_figure/stick_figureApp.swift
@@ -0,0 +1,17 @@
+//
+//  stick_figureApp.swift
+//  stick_figure
+//
+//  Created by Chris  on 2/8/25.
+//
+
+import SwiftUI
+
+@main
+struct stick_figureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
